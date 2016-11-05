ef5e330dc150e03233df7744fcae43d809c77fea
Brad <bradleynielsen@gmail.com>
2016-11-05 15:28:06 -0500

    third

---
+++ FriendFinder/app/server.js
@@ -0,0 +1,44 @@
+// take in results your users' surveys, then 
+
+// compare their answers with those from other users. 
+
+
+
+// The app will then display the name and picture of the user with the best overall match. 
+
+
+
+// * You should save the data in your app as an array of objects. Each of these objects should roughly follow the format below.
+
+// 	```
+// 	{
+// 	  "name":"Ahmed",
+// 	  "photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+// 	  "scores":[
+// 	     5,
+// 	     1,
+// 	     4,
+// 	     4,
+// 	     5,
+// 	     1,
+// 	     2,
+// 	     5,
+// 	     4,
+// 	     1
+// 	   ]
+// 	}
+// 	```
+
+var questions = [
+"How much state do you want in your life?",
+"Taxes or no?",
+"Drive yourself, or public chauffeur?",
+"BBQ or salad?",
+"On a scale of Trump to Hillary...? ",
+"Welfare or charity?",
+"Leave it to the states or feds?",
+"Eliminate the Federal Reserve, or bubbles?",
+"GMOs or natrual living?",
+"Do people own themselves, or the state own them?",
+"Taxation is theft (1=true)"
+]
