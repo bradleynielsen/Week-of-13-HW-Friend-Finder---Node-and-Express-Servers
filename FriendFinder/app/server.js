eb2e1913f865955573bb52763aed205e7d7fbef3
Brad <bradleynielsen@gmail.com>
2016-11-05 15:34:34 -0500

    going home is tired

--- FriendFinder/app/server.js
+++ FriendFinder/app/server.js
@@ -28,17 +28,35 @@
 // 	   ]
 // 	}
 // 	```
+// ### Instructions
+// 1. Your survey should have 10 questions of your choosing. Each answer should be on a scale of 1 to 5 based on how much the user agrees or disagrees with a question.
+
 
 var questions = [
-"How much state do you want in your life?",
-"Taxes or no?",
+"1",
+"Government is immoral.",
 "Drive yourself, or public chauffeur?",
-"BBQ or salad?",
+"BBQ is better than salad?",
 "On a scale of Trump to Hillary...? ",
 "Welfare or charity?",
 "Leave it to the states or feds?",
-"Eliminate the Federal Reserve, or bubbles?",
-"GMOs or natrual living?",
-"Do people own themselves, or the state own them?",
-"Taxation is theft (1=true)"
+"Eliminate the Federal Reserve.",
+"GMOs are evil?",
+"People own themselves.",
+"Taxation is theft."
 ]
+
+
+ // Determine the user's most compatible friend using the following as a guide:
+	// * Convert each user's results into a simple array of numbers (ex: `[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]`).
+	// * With that done, compare the difference between current user's scores against those from other users, question by question. Add up the differences to calculate the `totalDifference`.
+	// 	* Example: 
+	// 		* User 1: `[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]`
+	// 		* User 2: `[3, 2, 6, 4, 5, 1, 2, 5, 4, 1]`
+	// 		* Total Difference: **2 + 1 + 2 = ** ***5***
+	// * Remember to use the absolute value of the differences. Put another way: no negative solutions! Your app should calculate both `5-3` and `3-5` as `2`, and so on. 
+	// * The closest match will be the user with the least amount of difference.
+
+function compare(){
+	// for each element in [friends] loop over
+}
